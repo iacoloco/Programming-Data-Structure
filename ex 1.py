6367447a1fa3300ccb1d2e3693fd59a0f71b878b
iacoloco <iachiniarmando@gmail.com>
2024-03-19 16:16:43 +0000

    append student and add pets

--- ex 1.py
+++ ex 1.py
@@ -4,7 +4,13 @@
 student_pet_count_list = [0, 1, 5, 3, 7]
 num_student = len(student_pet_count_list)
 print("the student are", num_student)
- 
+#student 1 bough 2 pets
+student_pet_count_list[0] = student_pet_count_list[0] + 2
+print("student 1 bough:" ,student_pet_count_list[0]) 
+
+#add a student at the end(append) with 10 pets
+student_pet_count_list.append(10)  
+print(student_pet_count_list)
 #sum the number of pets
 sum=0
 for individual_pet_count in student_pet_count_list:
